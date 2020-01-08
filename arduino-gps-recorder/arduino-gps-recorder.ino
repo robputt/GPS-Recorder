4f9730b5d46732f3a7634e9eaee13843dc9dcea8
Robert Putt <rob@puttfamily.co.uk>
2020-01-08 21:05:39 +0000

    Update arduino-gps-recorder.ino

--- arduino-gps-recorder/arduino-gps-recorder.ino
+++ arduino-gps-recorder/arduino-gps-recorder.ino
@@ -1,159 +1,202 @@
+#include <SPI.h>
+#include "SdFat.h"
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-/*
-   This sample code demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
+
+// Requires TinyGPS++ - https://github.com/mikalhart/TinyGPSPlus
+// Requires SdFat - https://github.com/greiman/SdFat
+
 static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 
-// The TinyGPS++ object
+// TinyGPS++
 TinyGPSPlus gps;
-
-// The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
+
+// SD Card
+SdFat SD;
+#define SD_CS_PIN 10
+uint32_t nextWriteTime = 0;
+int downloadMode = 0;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   ss.begin(GPSBaud);
 
-  Serial.println(F("FullExample.ino"));
-  Serial.println(F("An extensive example of many interesting TinyGPS++ features"));
-  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println(F("by Mikal Hart"));
+  Serial.println(F("Starting GPS Recorder"));
+  Serial.println(F("Version 0.1"));
+  Serial.print(F("GPS Version: "));
+  Serial.println(TinyGPSPlus::libraryVersion());
+
   Serial.println();
-  Serial.println(F("Sats HDOP  Latitude   Longitude   Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum"));
-  Serial.println(F("           (deg)      (deg)       Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail"));
-  Serial.println(F("----------------------------------------------------------------------------------------------------------------------------------------"));
+  Serial.println("Waiting 30 seconds for computer connection to enter download mode...");
+  while (millis() < 5000) {
+    if (Serial.read() == 49) {
+      downloadMode = 1;
+    }
+  }
+  if (downloadMode) {
+    Serial.println("Download mode requested, entering download mode.");
+  } else {
+    Serial.println("Download mode not requested, entering recorder mode.");
+  }
+  Serial.println();
+
+  Serial.println("Initializing SD Card...");
+  if (!SD.begin(SD_CS_PIN)) {
+    Serial.println("SD card failed, or not present.");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("SD Card Initialized.");
+  Serial.println();
+
+  if (downloadMode) {
+    Serial.println("OK");
+  }
 }
 
 void loop()
 {
-  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+  if (downloadMode) {
+    runDownloadMode();
+  } else {
+    runRecorderMode();
+  }
+}
 
-  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
-  printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
-  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
-  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
-  printInt(gps.location.age(), gps.location.isValid(), 5);
-  printDateTime(gps.date, gps.time);
-  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
-  printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
-  printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
-  printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "*** ", 6);
+void runDownloadMode() {
+  String cmd = Serial.readString();
+  cmd.trim();
+  if (cmd == "ls") {
+    File dir = SD.open("/");
+    printDirectory(dir, true, false);
+    Serial.println();
+    Serial.println("OK");
+  }
+  if (cmd == "read") {
+    Serial.println("READ");
+    String fn = Serial.readString();
+    fn.trim();
+    File readfile = SD.open(fn);
+    if (readfile) {
+      while (readfile.available()) {
+        Serial.write(readfile.read());
+      }
+    } else {
+      Serial.println("ERR");
+    }
+    readfile.close();
+    Serial.println("OK");
+  }
+  if (cmd == "wipe") {
+    File dir = SD.open("/");
+    printDirectory(dir, false, true);
+    Serial.println("OK");
+  }
+}
 
-  unsigned long distanceKmToLondon =
-    (unsigned long)TinyGPSPlus::distanceBetween(
-      gps.location.lat(),
-      gps.location.lng(),
-      LONDON_LAT, 
-      LONDON_LON) / 1000;
-  printInt(distanceKmToLondon, gps.location.isValid(), 9);
+void printDirectory(File dir, bool prt, bool rem) {
+  if (prt) {
+    Serial.print("[");
+  }
+  while (true) {
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      if (prt) {
+        Serial.print("]");
+      }
+      break;
+    }
+    if (prt) {
+      Serial.print("\"");
+    }
+    char cname[30];
+    entry.getName(cname, 30);
+    String fname;
+    charToString(cname, fname);
+    if (prt) {
+      Serial.print(fname);
+      Serial.print("\",");
+    }
+    entry.close();
+    if (rem) {
+      if(!SD.remove(cname)) {
+        SD.rmdir(cname);
+      }
+    }
+  }
+}
 
-  double courseToLondon =
-    TinyGPSPlus::courseTo(
-      gps.location.lat(),
-      gps.location.lng(),
-      LONDON_LAT, 
-      LONDON_LON);
-
-  printFloat(courseToLondon, gps.location.isValid(), 7, 2);
-
-  const char *cardinalToLondon = TinyGPSPlus::cardinal(courseToLondon);
-
-  printStr(gps.location.isValid() ? cardinalToLondon : "*** ", 6);
-
-  printInt(gps.charsProcessed(), true, 6);
-  printInt(gps.sentencesWithFix(), true, 10);
-  printInt(gps.failedChecksum(), true, 9);
-  Serial.println();
-  
-  smartDelay(1000);
-
+void runRecorderMode() {
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read())) {
+      if(nextWriteTime <= millis()) {
+        nextWriteTime = millis() + 2000;
+        writeInfo();
+      }
+    }
   if (millis() > 5000 && gps.charsProcessed() < 10)
-    Serial.println(F("No GPS data received: check wiring"));
+  {
+    Serial.println(F("No GPS Detected"));
+    while(true);
+  }
 }
 
-// This custom version of delay() ensures that the gps object
-// is being "fed".
-static void smartDelay(unsigned long ms)
+void writeInfo()
 {
-  unsigned long start = millis();
-  do 
-  {
-    while (ss.available())
-      gps.encode(ss.read());
-  } while (millis() - start < ms);
-}
-
-static void printFloat(float val, bool valid, int len, int prec)
-{
-  if (!valid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartDelay(0);
-}
-
-static void printInt(unsigned long val, bool valid, int len)
-{
-  char sz[32] = "*****************";
-  if (valid)
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-  smartDelay(0);
-}
-
-static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
-{
-  if (!d.isValid())
-  {
-    Serial.print(F("********** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
-    Serial.print(sz);
-  }
+    String line = "";
+    if (gps.date.isValid())
+    {
+      line += gps.date.month();
+      line += "/";
+      line += gps.date.day();
+      line += "/";
+      line += gps.date.year();
+    }
+    else
+    {
+      line += "null";
+    }
   
-  if (!t.isValid())
-  {
-    Serial.print(F("******** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
-    Serial.print(sz);
-  }
-
-  printInt(d.age(), d.isValid(), 5);
-  smartDelay(0);
+    if (gps.time.isValid())
+    {
+      line += " ";
+      line += gps.time.hour();
+      line += ":";
+      line += gps.time.minute();
+      line += ":";
+      line += gps.time.second();
+    }
+    
+    line += ", ";
+    if (gps.location.isValid())
+    {
+      line += String(gps.location.lat(), 6);
+      line += ", ";
+      line += String(gps.location.lng(), 6);
+      line += ", ";
+      line += gps.altitude.meters();
+    }
+    else
+    {
+      line += "null, null, null";
+    }
+    Serial.println(line);
+    File dataFile = SD.open("1", FILE_WRITE);
+    if (dataFile) {
+      dataFile.println(line);
+    } else {
+      Serial.println("Error writing to SD card");
+    }
+    dataFile.close();
 }
 
-static void printStr(const char *str, int len)
+void charToString(char S[], String &D)
 {
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  smartDelay(0);
+ String rc(S);
+ D = rc;
 }
